50e619094582cae66ceaa46646c095570377cb79
travis <travis@nowhere>
2016-03-29 13:41:44 +0000

    travis: 30f6a41939a61b9c3e42304981119693e270bb11

--- app/mock-tricks.js
+++ app/mock-tricks.js
@@ -6,16 +6,27 @@ System.register([], function(exports_1, context_1) {
         setters:[],
         execute: function() {
             exports_1("TRICKS", TRICKS = [
-                { "id": 1, "name": "AS" },
-                { "id": 2, "name": "TS" },
-                { "id": 3, "name": "EB" },
-                { "id": 4, "name": "CL" },
-                { "id": 5, "name": "Criss-Cross" },
-                { "id": 6, "name": "Double Under" },
-                { "id": 7, "name": "One-hand Release" },
-                { "id": 8, "name": "TJ" },
-                { "id": 9, "name": "Caboose" },
-                { "id": 10, "name": "Loop" }
+                { "id": 1, "level": "Basics", "subs": [
+                        { "id2": 1, "name": "180 Backwards" },
+                        { "id2": 2, "name": "180 Forward" },
+                        { "id2": 3, "name": "360" },
+                        { "id2": 4, "name": "Backwards Jump" },
+                        { "id2": 5, "name": "Backwards Side Swing" },
+                        { "id2": 6, "name": "Backwards Side Swing Open" },
+                        { "id2": 7, "name": "Bell" },
+                        { "id2": 8, "name": "Criss Cross" },
+                        { "id2": 9, "name": "Double Bounce" },
+                        { "id2": 10, "name": "Double Side Swing" }
+                    ]
+                },
+                { "id": 2, "level": "Level 1", "subs": [
+                        { "id2": 1, "name": "Double Under", "type": "Multiple" },
+                        { "id2": 2, "name": "Trick", "type": "Multiple" },
+                        { "id2": 3, "name": "Trick", "type": "Power" },
+                        { "id2": 4, "name": "Trick", "type": "Manipulation" },
+                        { "id2": 5, "name": "Trick", "type": "Release" }
+                    ]
+                }
             ]);
         }
     }
